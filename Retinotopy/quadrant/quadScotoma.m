31b9a4bf7a03615a2fbb8a5addd864602f7667db
Brian Wandell <wandell@stanford.edu>
2020-07-16 21:15:07 -0700

    More comments.  Fixed the rng() for the Shuffled condition to 100

--- Retinotopy/quadrant/quadScotoma.m
+++ Retinotopy/quadrant/quadScotoma.m
@@ -1,13 +1,16 @@
-%% retStimulusEdit - Explanation and methods for editing the stimulus
+%% quadScotoma - 
 % 
-% Start with 8bars or with the Shuffled version and make a scotoma
+% Start with 8bars (sweep) or with the shuffled version and apply a
+% scotoma to the upper right quadrant.
 %
-% This is a hard scotoma
+% We apply a hard scotoma.  Maybe I should have blurred it.
 %
+% See also
 %
 
 
 %% load('8bars_AS_softEdge.mat','stimulus');
+
 load('8bars.mat','stimulus');
 disp(stimulus)
 
@@ -23,21 +26,25 @@ for ii=1:size(stimulus.images,3)
     images2(rows,cols,ii) = uniform;
 end
 
-% Everything is the same but the positions are randomized
+%% Show the scotoma
+
 for ii=1:size(images2,3)
     image(images2(:,:,ii)); 
     pause(0.1);
 end
 
-%% Everything is the same except the bar positions have been randomized
+%% Write it out.  Sweep and Upper Right Scotoma
 
 stimulus.images = images2;
-
 p = fileparts(which('8bars.mat'));
 fname = fullfile(p,'8barsUpperRightScotoma');
+fprintf('Saved in storedImageMatrices %s\n',fname);
+
 save(fname,'stimulus');
 
 %% load('8bars_AS_softEdge.mat','stimulus');
+
+% Now apply a scotoma to the shuffled bars, not the sweep.
 load('8barsShuffled.mat','stimulus');
 disp(stimulus)
 
@@ -57,10 +64,11 @@ end
 %%  Save it
 
 stimulus.images = images2;
-
 p = fileparts(which('8bars.mat'));
 fname = fullfile(p,'8barsShuffledUpperRightScotoma.mat');
+fprintf('Saved in storedImageMatrices %s\n',fname);
+
 save(fname,'stimulus');
 
-%%
+%% END
 
--- Retinotopy/quadrant/quadShuffleBar.m
+++ Retinotopy/quadrant/quadShuffleBar.m
@@ -1,15 +1,12 @@
-%% retStimulusEdit - Explanation and methods for editing the stimulus
+%% quadShuffleBar - Explanation and methods for editing the 8bars stimulus
 %
-% The purpose is to clarify the entries of the 'stimulus' struct that
+% This script comments on the entries of the 'stimulus' struct that
 % contains the images and sequence for a standard scan.
 %
-% Then we modify the parameters to achieve two goals.  
+% Then we modify the parameters to shuffle the order of bar positions
+% rather than sweep.
 %
-%  First we want to shuffle the order of bar positions rather than
-%  sweep.
-%
-%  Second, we want to blank out certain regions of the screen so that
-%  no contrast appears there (artificial scotoma).
+% See quadScotoma for applying a scotoma to these stimuil
 %
 % Parameter definitions of the stimulus struct
 %
@@ -29,11 +26,12 @@
 
 
 %% load('8bars_AS_softEdge.mat','stimulus');
+
 load('8bars.mat','stimulus');
 disp(stimulus)
 
 
-%% Timing
+%% These parameters document the timing of the display
 
 % Start the first frame at time 0
 stimulus.seqtiming(1)
@@ -51,24 +49,10 @@ totalDuration = stimulus.seqtiming(end) + timeStep % Start last stimulus and the
 % is presented for this much time
 stimulusDuration = totalDuration - 12   % This many seconds of warm up
 
-%{
-% If each position has 8 contrast patterns
-nImages = size(stimulus.images,3);
-for ii=1:nImages
-    image(stimulus.images(:,:,ii)); pause(0.1);
-    if ~mod(ii,8)
-        disp(ii)
-    end
-end
-
-% The last image is blank
-image(stimulus.images(:,:,end))
-%}
-
 %% Time at a position
 
-% Frame the values in 8bars.mat, we have 2 sec TR, so 32 frames at one
-% position.  Let's have a look.  
+% In the quadrantanopia experiments we use a 2 sec TR, so 32 frames.
+% All of these frames represent a stimulus at one position.
 
 % The total stimulus sequence
 nFrames = numel(stimulus.seq);
@@ -78,21 +62,26 @@ starts = 1:32:nFrames;
 
 % Pull out a sequence with one frame prior to this position and one
 % frame after
-s = 5;
+s = 20;     % Many of these starts are the blank frames.
 thisSeq = (starts(s)-1):starts(s+1);
 plot(stimulus.seq(thisSeq),'-o');
 
-% Look at the images to see that we are right
+%% Visualize 
+
+% Look at the images to see that we are right.  First one is offset,
+% then 32 frames at one position, then the next position.
 nImages = numel(thisSeq);
 imgSeq = stimulus.images(:,:,stimulus.seq(thisSeq));
-for ii=1:nImages
-    image(imgSeq(:,:,ii)); pause(0.2);
+image(imgSeq(:,:,1)); pause(0.3);
+for ii=2:nImages
+    image(imgSeq(:,:,ii)); pause(0.1);
 end
+image(imgSeq(:,:,end));
 
-%% Suppose we want to randomize the timing of the positions
+%% Shuffle (randomize) the timing of the bar positions
 
-% Then we we can randomly exchange pairs of the 8 blocks images.
-% There are
+% We can randomly exchange pairs of the 8 blocks images.
+% There are this many positions.
 nPositions = 384/8;
 
 % The sequence of images at a position start at these blocks
@@ -105,13 +94,17 @@ nImages = 8;
 imgSeq = stimulus.images(:,:,thisSeq);
 for ii=1:nImages
     image(imgSeq(:,:,ii)); 
-    pause(0.2);
+    pause(0.1);
 end
 
 %% Now exchange blocks of 8
 
+rng(100);
+
+% We will do the exchange in this temp variable, images2.
 images2 = stimulus.images;
 
+% We do 50 exchanges on 47 bar positions
 for ii=1:50
     thisPair = randi(47,[1,2]);
     seq1 = pStarts(thisPair(1)):(pStarts(thisPair(1)+1)-1);
@@ -134,8 +127,10 @@ end
 stimulus.images = images2;
 
 %%  Everything is the same except the bar positions have been randomized
+
 p = fileparts(which('8bars.mat'));
 fname = fullfile(p,'8barsShuffled');
+fprintf('Saving %s\n',fname);
 save(fname,'stimulus');
 
 %% Check that it loads and runs
@@ -146,7 +141,5 @@ for ii=1:length(stimulus.seq)
     image(stimulus.images(:,:,stimulus.seq(ii))); 
     pause(0.05);
 end
-
-%% Now create a version with a scotoma in the upper right
 
 %% END
